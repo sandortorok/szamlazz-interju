f4b31886b498fbd9f537ae28b1631885f85488e8
sandortorok <sandort99@gmail.com>
2026-01-21 13:42:35 +0100

    cleaning up unnecesary env files

--- probafeladat-frontend/src/environments/environment.example.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: '__API_URL__',
-  szamlazzCredentials: {
-    apiKey: '__SZAMLAZZ_API_KEY__',
-  },
-};
---
+++ probafeladat-frontend/src/environments/environment.template.ts
@@ -0,0 +1,11 @@
+// Ez a template fájl - NE MÓDOSÍTSD KÖZVETLENÜL!
+// Docker build során a Dockerfile lecseréli a __PLACEHOLDER__ értékeket
+// Lokális fejlesztéshez másold environment.ts néven és töltsd ki
+
+export const environment = {
+  production: true,
+  apiUrl: '__API_URL__',
+  szamlazzCredentials: {
+    apiKey: '__SZAMLAZZ_API_KEY__',
+  },
+};
